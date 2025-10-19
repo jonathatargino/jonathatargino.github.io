f2390ea29836eae6037bbb5f148c433b8081f8cf
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-10-19 17:31:12 -0300

    fix: route paths, centered layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jonathatargino.github.io",
-  base: "/jonathatargino",
   integrations: [mdx(), sitemap()],
 });
